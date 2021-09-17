575f8e4f166cde328a65fee1e2524ddad1a574de
mhaines10 <mleehaines@gmail.com>
2021-09-17 11:08:49 -0500

    Sort Added

--- src/Models/Person.ts
+++ src/Models/Person.ts
@@ -1,4 +1,7 @@
+import internal from "stream";
+
 export interface Person {
+    id: number,
     name: string,
     desc: string,
     avatar: string
--- src/Services/GetPeople.ts
+++ src/Services/GetPeople.ts
@@ -3,7 +3,9 @@ import { Person } from '../Models/Person';
 
 export const GetPeople = (queryString: string): Person[] => {
     let filteredList: Person[] = mockData.filter(x => x.name.toLowerCase().startsWith(queryString.toLowerCase())).map((x) => (
-        {name: x.name, desc: x.description, avatar: x.avatar}
-    ));
+        {name: x.name, desc: x.description, avatar: x.avatar, id: x.id}
+    )).sort((a, b) => {
+        return a.name.localeCompare(b.name);
+    });
     return filteredList;
 }
